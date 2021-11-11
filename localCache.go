0dd8fa0790d3fcae66267356a65f2a9746958a33
Patrick Garziglia <pat@pgarz.com>
2021-11-10 23:33:50 -0500

    rebuild cache from mongodb on restart

--- localCache.go
+++ localCache.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func buildCache(collection *mongo.Collection) (map[int]User, error) {
+	leaderboardCache := make(map[int]User)
+
+	findOpts := options.Find()
+	findOpts.SetProjection(bson.M{
+		"_id":      0,
+		"rlUserId": 1,
+		"wins":     1,
+		"losses":   1,
+		"elo":      1,
+		"rank":     1,
+	})
+
+	response, err := collection.Find(context.TODO(), bson.M{}, findOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// response is read in as a "cursor" that iterates over each individual document
+	for response.Next(context.TODO()) {
+
+		// create a value into which the single document can be decoded
+		var user User
+		err := response.Decode(&user)
+		if err != nil {
+			log.Fatal(err)
+		}
+		leaderboardCache[user.RlUserId] = User{
+			RlUserId: user.RlUserId,
+			Wins:     user.Wins,
+			Losses:   user.Losses,
+			Elo:      user.Elo,
+			Rank:     user.Rank,
+		}
+	}
+
+	if err := response.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the cursor once finished
+	response.Close(context.TODO())
+
+	return leaderboardCache, nil
+}
+
 func cacheCompare(leaderboardCache map[int]User, users []User) (map[int]User, []User, error) {
 
 	var changedUsers []User
